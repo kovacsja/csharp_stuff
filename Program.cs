b318f0e93fc114697f137ac536622dcba7c1d577
kovacsja <janos.kovacs.h@gmail.com>
2018-09-19 22:57:57 +0200

    0.1 version

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace c_sharp_date_goodies
+{
+    class Program
+    {
+
+		static void Main(string[] args)
+        {
+            DateTime dateToConvert = DateTime.Now;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(dateToConvert);
+            string datetext = DateTime.Now.ToShortDateString();
+            System.Console.WriteLine(datetext + DateTime.Now.ToShortTimeString());
+        }
+    }
+}
