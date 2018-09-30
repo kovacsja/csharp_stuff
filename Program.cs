366828cdb901d18d278f0f28486cb2f48a5ed3e4
kovacsja <janos.kovacs.h@gmail.com>
2018-09-30 18:19:27 +0200

    ver 0.1

--- Program.cs
+++ Program.cs
@@ -5,14 +5,28 @@ namespace c_sharp_date_goodies
 {
     class Program
     {
-
 		static void Main(string[] args)
         {
-            DateTime dateToConvert = DateTime.Now;
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(dateToConvert);
-            string datetext = DateTime.Now.ToShortDateString();
-            System.Console.WriteLine(datetext + DateTime.Now.ToShortTimeString());
+            System.Console.WriteLine(NowToPostFix());
+            System.Console.WriteLine(DateToPostFix());
+        }
+
+        static string NowToPostFix () {
+            string YearPart = Convert.ToString(DateTime.Now.Year);
+            string MonthPart = "0" + Convert.ToString(DateTime.Now.Month);
+            string DayPart = "0" + Convert.ToString(DateTime.Now.Day);
+            string HourPart = "0" + Convert.ToString(DateTime.Now.Hour);
+            string MinPart = "0" + Convert.ToString(DateTime.Now.Minute);
+
+            return YearPart + MonthPart.Substring(MonthPart.Length - 2) + DayPart.Substring(DayPart.Length -2) + HourPart.Substring(HourPart.Length -2) + MinPart.Substring(MinPart.Length -2);
+        }
+        static string DateToPostFix() {
+            string YearPart = Convert.ToString(DateTime.Now.Year);
+            string MonthPart = "0" + Convert.ToString(DateTime.Now.Month);
+            string DayPart = "0" + Convert.ToString(DateTime.Now.Day);
+
+            return YearPart + MonthPart.Substring(MonthPart.Length - 2) + DayPart.Substring(DayPart.Length -2);
+
         }
     }
 }
