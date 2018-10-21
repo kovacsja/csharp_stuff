f66d2758c29673de43281ce5f17f52ac8b07fa99
kovacsja <janos.kovacs.h@gmail.com>
2018-10-21 18:31:48 +0200

    name fixes

--- Program.cs
+++ Program.cs
@@ -7,25 +7,25 @@ namespace c_sharp_date_goodies
     {
 		static void Main(string[] args)
         {
-            System.Console.WriteLine(NowToPostFix());
-            System.Console.WriteLine(DateToPostFix());
+            System.Console.WriteLine(NowPostFix());
+            System.Console.WriteLine(TodayPostFix());
         }
 
-        static string NowToPostFix () {
+        static string NowPostFix () {
             string YearPart = Convert.ToString(DateTime.Now.Year);
             string MonthPart = "0" + Convert.ToString(DateTime.Now.Month);
             string DayPart = "0" + Convert.ToString(DateTime.Now.Day);
             string HourPart = "0" + Convert.ToString(DateTime.Now.Hour);
             string MinPart = "0" + Convert.ToString(DateTime.Now.Minute);
 
-            return YearPart + MonthPart.Substring(MonthPart.Length - 2) + DayPart.Substring(DayPart.Length -2) + HourPart.Substring(HourPart.Length -2) + MinPart.Substring(MinPart.Length -2);
+            return "_" + YearPart + MonthPart.Substring(MonthPart.Length - 2) + DayPart.Substring(DayPart.Length -2) + HourPart.Substring(HourPart.Length -2) + MinPart.Substring(MinPart.Length -2);
         }
-        static string DateToPostFix() {
+        static string TodayPostFix() {
             string YearPart = Convert.ToString(DateTime.Now.Year);
             string MonthPart = "0" + Convert.ToString(DateTime.Now.Month);
             string DayPart = "0" + Convert.ToString(DateTime.Now.Day);
 
-            return YearPart + MonthPart.Substring(MonthPart.Length - 2) + DayPart.Substring(DayPart.Length -2);
+            return "_" + YearPart + MonthPart.Substring(MonthPart.Length - 2) + DayPart.Substring(DayPart.Length -2);
 
         }
     }
